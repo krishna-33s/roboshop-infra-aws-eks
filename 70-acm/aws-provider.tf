8b193d76c5d6c4aeb05c193897538cd9cb4aaced
krishna <venkatakrishnaredd450@gmail.com>
2026-08-21 13:40:58 +0530

    terraform eks

--- 70-acm/aws-provider.tf
+++ 70-acm/aws-provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "venkat-dev-88s"
-    key    = "terraform-eks-frontend-alb"
+    key    = "terraform-eks-acm"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
