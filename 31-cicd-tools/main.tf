35fc5f8d51daae4e0c9e9452050cbaadc0f18f27
krishna <venkatakrishnaredd450@gmail.com>
2026-09-03 18:33:01 +0530

    jenkins

--- 31-cicd-tools/main.tf
+++ 31-cicd-tools/main.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "sonarqube" {
   key_name = "krishna-88s"
   # need more for terraform
   root_block_device {
-    volume_size = 20
+    volume_size = 50
     volume_type = "gp3"
   }
   tags = merge(
