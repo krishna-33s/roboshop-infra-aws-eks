957002d29dc77fb37b3243846274efd44d0ce2d3
krishna <venkatakrishnaredd450@gmail.com>
2026-09-03 16:42:51 +0530

    jenkins

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -51,6 +51,6 @@ data "aws_ssm_parameter" "jenkins_agent_sg_id" {
 }
 
 data "aws_ssm_parameter" "sonarqube_sg_id" {
-    name = "/${var.project}/${var.env}/sonar_sg_id"
+    name = "/${var.project}/${var.env}/sonarqube_sg_id"
 }
 
